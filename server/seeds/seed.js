ad9fc6345f6e168a547482c1fcf3b9640a174111
J <jamscc22@gmail.com>
2023-03-13 19:33:13 -0400

    model - draft Share; seed and starting data (draft)

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,31 +1,71 @@
-const db = require('../config/connection');
+const { users, notes, sharedCategory } = require('./startingData');
 const Note = require('../models/Note');
+const Share = require('../models/Share');
 const User = require('../models/User');
-const { users, notes } = require('./startingData');
+const connection = require('../config/connection');
 
-db.once('open', async () => {
+const cn = 'open';
+const re = 'error';
+
+// starting data
+const startingData = async () => {
     await User.deleteMany({});
     await Note.deleteMany({});
-
-    await User.insertMany(users);
+    await Share.deleteMany({});
+    await User.create(users);
     await Note.insertMany(notes);
+    await Share.insertMany(sharedCategory);
 
+    // Note find
+    // Share find
     const allNotes = await Note.find({});
-    
+    const sharedCategories = await Share.find({});
+
     const notesUser = allNotes.map((data) => {
         return {
             _id: data._id,
-            username: data.username
+            userName: data.userName,
+            shared: data.shared,
+            category: data.category.toLowerCase()
         }
     })
 
-    for (let i = 0; i < notesUser.length; i++) {
-        await User.findOneAndUpdate(
-            { username: notesUser[i].username },
-            { $push: { notes: notesUser[i]._id } }
+    // Share findOneAndUpdate
+    for (let h = 0; h < sharedCategories.length; h++) {
+        await Share.findOneAndUpdate(
+            { sharedCategory: sharedCategories[h].sharedCategory },
+            { $set: { sharedCategory: sharedCategories[h].sharedCategory.toLowerCase() } },
+            { new: true }
         )
     }
 
+    // Share find
+    const sharedData = await Share.find({});
+
+    for (let i = 0; i < notesUser.length; i++) {
+        // User findOneAndUpdate
+        await User.findOneAndUpdate(
+            { userName: notesUser[i].userName },
+            { $push: { notes: notesUser[i]._id } }
+        )
+
+        for (let j = 0; j < sharedData.length; j++) {
+            if (notesUser[i].shared === true && notesUser[i].category === sharedData[j].sharedCategory) {
+                // User findOneAndUpdate
+                await User.findOneAndUpdate(
+                    { userName: notesUser[i].userName },
+                    { $push: { sharedUserNotes: notesUser[i]._id } }
+                )
+                // Share findOneAndUpdate
+                await Share.findOneAndUpdate(
+                    { sharedCategory: notesUser[i].category },
+                    { $push: { sharedNotes: notesUser[i]._id } }
+                )
+            }
+        }
+    }
     console.info('starting data - added');
     process.exit(0);
-});
+}
+
+connection.on(re, (e) => e).once(cn, startingData);
--- server/seeds/startingData.js
+++ server/seeds/startingData.js
@@ -1,31 +1,46 @@
 const users = [
     {
-        username: "drestivo",
-        email: "davidr@gmail.com",
-        password: "Bootcamp@2",
-        notes: []
-    },
-    {
-        username: "jamesC",
-        email: "jamesc@hotmail.com",
-        password: "Bootcamp@3",
-        notes: []
+        firstName: "David",
+        userName: "drestivo",
+        userEmail: "davidr@gmail.com",
+        userPwd: "Bootcamp@2",
+        question: "question",
+        answer: "answer",
+        notes: [],
+        sharedUserNotes: []
     }
 ];
 
 const notes = [
     {
         category: "html",
-        text: "link to my project 1 with useful html",
-        link: " https://github.com/davidrestivo/project-1-columbia ",
-        username: "drestivo"
+        noteInput: "link to my project 1 with useful html",
+        link: "https://github.com/davidrestivo/project-1-columbia",
+        userName: "drestivo",
+        shared: false
     },
     {
-        category: " css ",
-        text: "link to my project 2 with some great css",
+        category: "css",
+        noteInput: "link to my project 2 with some great css",
         link: "https://github.com/davidrestivo/4-Real-Estate",
-        username: "jamesC"
+        userName: "drestivo",
+        shared: true
     }
 ];
 
-module.exports = { users, notes }
+const sharedCategory = [
+    {
+        sharedCategory: 'HTML',
+        sharedNotes: []
+    },
+    {
+        sharedCategory: 'CSS',
+        sharedNotes: []
+    },
+    {
+        sharedCategory: 'JavaScript',
+        sharedNotes: []
+    }
+]
+
+module.exports = { users, notes, sharedCategory }
